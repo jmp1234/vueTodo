7bef1070a5f420d4dd5ff51adaad779aa2c6bfa6
jmp1234 <johnnypham944@yahoo.com>
2019-08-28 12:27:02 -0700

    completed todo list next step is to connect to json placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos='todos'
+      v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Todos from './components/todos';
+import AddTodo from './components/addTodo';
+import Header from './components/layout/header';
 
 export default {
   name: 'app',
   components: {
-  }
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'todo 1',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'todo 2',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'todo 3',
+          completed: false
+        },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo]
+    },
+   }
 }
 </script>
 
@@ -22,5 +61,19 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+
   }
 </style>
